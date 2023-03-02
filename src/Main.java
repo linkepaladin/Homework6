1a30ea4a924d5fd3be63512959aa21579f65c3f6
Linke <linke@duck.com>
2023-03-03 01:45:13 +0300

    task 7 completed

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
         public  static void task1() {
             for (int i = 1; i <= 10; i++) {
@@ -34,6 +35,11 @@ public class Main {
         }
         public static void task6() {
             for (int i = 7; i <= 98; i = i + 7) {
+                System.out.println(i);
+            }
+        }
+        public static void task7() {
+            for (int i = 1; i <= 512; i = i * 2) {
                 System.out.println(i);
             }
         }
