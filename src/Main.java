4fc76976b5ebc2a594d175bf290c293f2a922e9b
Linke <linke@duck.com>
2023-03-03 01:26:17 +0300

    task 1 and task 2 completed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
         public  static void task1() {
             for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        }
+        public static void task2() {
+            for (int i = 10; i >= 1; i--) {
                 System.out.println(i);
             }
         }
