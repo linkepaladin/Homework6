b3bb7c9592567128a7a037ad824ed797e9604425
Linke <linke@duck.com>
2023-03-03 01:31:09 +0300

    task 3 completed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
         public  static void task1() {
             for (int i = 1; i <= 10; i++) {
@@ -10,6 +11,11 @@ public class Main {
         }
         public static void task2() {
             for (int i = 10; i >= 1; i--) {
+                System.out.println(i);
+            }
+        }
+        public static void task3() {
+            for (int i = 0; i <= 17; i = i + 2) {
                 System.out.println(i);
             }
         }
