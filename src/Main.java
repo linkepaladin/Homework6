4542800dd87770a1094fc772fa74260c5b423675
linke <linke@duck.com>
2025-05-12 01:43:53 +0300

    Complete Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        // task 1
+
+        System.out.println("Задача 1");
+
+        int deposit = 15000;
+        int savings = 0;
+        int goal = 2_459_000;
+        int month = 0;
+        while (savings <= goal) {
+            savings = savings + deposit;
+            month = month + 1;
+            if (savings >= goal) {
+                System.out.println("Месяц " + month + " сумма накоплений равна " + savings + " рублей");
+            }
         }
+    }
 }
