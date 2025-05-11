90e931ee8a17842d015ebcf4028af3d8170ec1ee
linke <linke@duck.com>
2025-05-12 02:06:47 +0300

    Complete Task 3

--- src/Main.java
+++ src/Main.java
@@ -27,11 +27,21 @@ public class Main {
         for (int i = 10; i > 0; i--) {
             System.out.print(i + " ");
         }
+        System.out.println();
 
         // task 3;
 
         System.out.println("Задача 3");
 
-
+        int population = 12_000_000;
+        int fertility = (population * 17) / 1000;
+        int mortality = (population * 8) / 1000;
+        int populationIncrease = fertility - mortality;
+        int firstYear = 0;
+        while (firstYear < 10) {
+            population = population + populationIncrease;
+            firstYear++;
+            System.out.println("Год " + firstYear + " численность населения составляет " + population);
+        }
     }
 }
